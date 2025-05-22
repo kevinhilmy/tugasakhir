a2657069b48bd5aa9bf4df58e51dc06ef4228340
Ersamey <meiliaersa@gmail.com>
2025-05-22 16:50:36 +0700

    login

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <h1>Login disini</h1>
+    <input type="text" name="username" placeholder="Username" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+    <p>Belum punya akun? <a href="register.php">Daftar disini</a></p>
+    <p>Sudah punya akun? <a href="forgot_password.php">Lupa password?</a></p>
+
+</body>
+
+</html>
