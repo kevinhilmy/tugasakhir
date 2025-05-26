156f393d68c9bb0926e56869637d49d0ddb16938
KeyshaNaurah <keyyakunke3@gmail.com>
2025-05-26 15:28:23 +0700

    history

--- history.php
+++ history.php
@@ -3,9 +3,220 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
+    <title>History</title>
+   <style>
+      h1 {
+          padding-left: 70px;
+      }
+      table {
+            border-collapse: collapse;
+            margin-left: auto;
+            margin-right: auto;
+        }
+      table, th, td {
+            border: 1px solid black;
+        }
+      th, td {
+            padding: 17px;
+            font-size: 18px;
+        }
+   </style>
 </head>
 <body>
-    hi 
+   <br><br><br>
+    <table style="width: 1250px" border="1">
+       <h1> Riwayat pesanan </h1>
+       <tr>
+         <th>
+            ID Akun
+         </th>   
+         <th>
+            ID Pesanan
+         </th>   
+         <th>
+            Tanggal
+         </th>   
+         <th>
+            Total Produk
+         </th>   
+         <th>
+            Total Harga
+         </th>   
+         <th>
+            Bayar
+         </th>   
+         <th>
+            Kembali
+         </th>   
+       </tr>
+
+       <tr align="center">
+         <td> 
+            kasir1
+         </td>   
+         <td> 
+            123451
+         </td>  
+         <td> 
+            15-06-2025
+         </td>  
+         <td> 
+            2
+         </td>  
+         <td> 
+            53.000
+         </td>   
+         <td> 
+            55.000
+         </td>   
+         <td> 
+            2.000
+         </td>   
+       </tr>
+
+       <tr align="center">
+         <td> 
+            kasir1
+         </td>   
+         <td> 
+            123452
+         </td>  
+         <td> 
+            15-06-2025
+         </td>  
+         <td> 
+            1
+         </td>  
+         <td> 
+            25.000
+         </td>   
+         <td> 
+            25.000
+         </td>   
+         <td> 
+            -
+         </td>   
+       </tr>
+
+       <tr align="center">
+         <td> 
+            kasir1
+         </td>   
+         <td> 
+            123453
+         </td>  
+         <td> 
+            15-06-2025
+         </td>  
+         <td> 
+            1
+         </td>  
+         <td> 
+            17.000
+         </td>   
+         <td> 
+            20.000
+         </td>   
+         <td> 
+            3.000
+         </td>   
+       </tr>
+
+       <tr align="center">
+         <td> 
+            kasir2
+         </td>   
+         <td> 
+            123454
+         </td>  
+         <td> 
+            15-06-2025
+         </td>  
+         <td> 
+            5
+         </td>  
+         <td> 
+            113.000
+         </td>   
+         <td> 
+            120.000
+         </td>   
+         <td> 
+            7.000
+         </td>   
+       </tr>
+
+       <tr align="center">
+         <td> 
+            kasir2
+         </td>   
+         <td> 
+            123455
+         </td>  
+         <td> 
+            15-06-2025
+         </td>  
+         <td> 
+            3
+         </td>  
+         <td> 
+            51.000
+         </td>   
+         <td> 
+            51.000
+         </td>   
+         <td> 
+            -
+         </td>   
+       </tr>
+    
+       <tr align="center">
+         <td> 
+            kasir2
+         </td>   
+         <td> 
+            123456
+         </td>  
+         <td> 
+            15-06-2025
+         </td>  
+         <td> 
+            2
+         </td>  
+         <td> 
+            50.000
+         </td>   
+         <td> 
+            50.000
+         </td>   
+         <td> 
+            -
+         </td>   
+       </tr>
+
+       <tr align="center">
+         <td> 
+            kasir3
+         </td>   
+         <td> 
+            123457
+         </td>  
+         <td> 
+            15-06-2025
+         </td>  
+         <td> 
+            2
+         </td>  
+         <td> 
+            49.000
+         </td>   
+         <td> 
+            50.000
+         </td>   
+         <td> 
+            1.000
+         </td>   
+       </tr>
+    </table>
 </body>
 </html>
