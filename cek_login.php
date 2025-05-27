7853feb17c78d7cfa0cbb66f15b09348439828d8
ipah-rahmah <ipahsyamilah1106@gmail.com>
2025-05-27 14:05:44 +0700

    koneksi lagi

--- cek_login.php
+++ cek_login.php
@@ -2,26 +2,22 @@
 session_start();
 
 // Koneksi ke database
-$koneksi = new mysqli("localhost", "root", "", "nama_database");
+include 'koneksi.php';
 
-// Cek koneksi
-if ($koneksi->connect_error) {
-    die("Koneksi gagal: " . $koneksi->connect_error);
-}
 
 // Tangkap data dari form
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 // Cek data dari database
-$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-$result = $koneksi->query($sql);
+$sql = "SELECT * FROM tb_akun WHERE username='$username' AND password='$password'";
+$result = $db->query($sql);
 
 if ($result->num_rows > 0) {
     // Login berhasil
     $_SESSION['username'] = $username;
     $_SESSION['status'] = "login";
-    header("location:dashboard.php");
+    header("location:menu.php");
 } else {
     // Login gagal
     echo "Username atau Password salah!";
