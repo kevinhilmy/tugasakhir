08c58e686560a8c952ab4249cd33a5535a0cd6d3
kevinhilmy <kevinhilmy.r@gmail.com>
2025-06-03 16:03:06 +0700

    commit

--- akun.php
+++ akun.php
@@ -9,7 +9,7 @@ include 'koneksi.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="admin.css">
+    <!-- <link rel="stylesheet" href="admin.css"> -->
     <link rel="shortcut icon" href="" type="image/x-icon">
     <script>
         /* Set the width of the sidebar to 250px (show it) */
@@ -22,6 +22,140 @@ include 'koneksi.php';
         document.getElementById("mySidepanel").style.width = "0";
         }
     </script>
+    <style>
+        *{
+        margin: 0;
+        padding: 0;
+        }
+
+        body{
+        background-color: #736153;
+        }
+
+        .nav{
+        display: flex;
+        justify-content: space-between;
+        padding: 15px;
+        background-color: #bb874f;
+        }
+
+        .logo{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        }
+
+        .logo a{
+        text-decoration: none;
+        color: #111;
+        }
+
+        a img{
+        width: 50px;
+        }
+
+        .sidepanel {
+        height: 250px; /* Specify a height */
+        width: 0; /* 0 width - change this with JavaScript */
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Stay on top */
+        top: 0;
+        right: 0;
+        background-color: #111; /* Black*/
+        overflow-x: hidden; /* Disable horizontal scroll */
+        padding-top: 60px; /* Place content 60px from the top */
+        transition: 0.5s; /* 0.5 second transition effect to slide in the sidepanel */
+        }
+
+        /* The sidepanel links */
+        .sidepanel a {
+        padding: 8px 8px 8px 32px;
+        text-decoration: none;
+        font-size: 25px;
+        color: #818181;
+        display: block;
+        transition: 0.3s;
+        }
+
+        /* When you mouse over the navigation links, change their color */
+        .sidepanel a:hover {
+        color: #f1f1f1;
+        }
+
+        /* Position and style the close button (top right corner) */
+        .sidepanel .closebtn {
+        position: absolute;
+        top: 0;
+        right: 25px;
+        font-size: 36px;
+        margin-left: 50px;
+        }
+
+        /* Style the button that is used to open the sidepanel */
+        .openbtn {
+        font-size: 20px;
+        cursor: pointer;
+        background-color: transparent;
+        color: black;
+        padding: 10px 15px;
+        border: none;
+        }
+
+        .openbtn:hover {
+        background-color: #444;
+        }
+
+        .content{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        /* align-items: center; */
+        margin: 3%;
+        /* padding: 3%; */
+        background-color: #ffe7cd;
+        }
+
+        .content h1{
+        padding: 10px;
+        }
+
+        table, th, td{
+        border: solid 1px transparent;
+        border-collapse: collapse;
+        padding: 10px;
+        text-align: center;
+        }
+
+        table{
+        margin-top: 10px;
+        }
+
+        tr:nth-child(even) {
+        background-color: #edd7bf;
+        }
+
+        .aksi{
+        display: flex;
+        justify-content: center;
+        }
+
+        .aksi a{
+        text-decoration: none;
+        color: black;
+        }
+
+        input[type = "button"]{
+        padding: 10px;
+        border-radius: 15px;
+        border: solid 1px black;
+        }
+
+        .edit{
+        /* margin-top: 10px; */
+        padding: 10px;
+        }
+    </style>
 </head>
 <body>
     <div class="nav">
@@ -61,7 +195,7 @@ include 'koneksi.php';
                 <td>kevinaja</td>
                 <td>kevinhilmy.r@gmial.com</td>
                 <td>kasir</td>
-                <td class="aksi"><input type="button" value="edit"><input type="button" value="hapus"></td>
+                <td class="aksi"><a href="kelola.php">edit</a></td>
             </tr>
             <tr>
                 <td>2</td>
