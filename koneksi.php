52488448a12deb3d781633de274a67564d301944
Ersamey <meiliaersa@gmail.com>
2025-06-05 11:15:16 +0700

    func akun

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,16 @@
 <?php
-$db=mysqli_connect("localhost","root","","db_kasir");
+$db = mysqli_connect("localhost", "root", "", "db_kasir");
 if (!$db) {
     die("Connection failed: " . mysqli_connect_error());
 }
-?>
+
+// ambil seluruh data dari tabel admin 
+function pilih($tabel, $kondisi = null)
+{
+    global $db;
+    $query = "SELECT * FROM $tabel";
+    if ($kondisi) {
+        $query .= " WHERE $kondisi";
+    }
+    return mysqli_query($db, $query);
+}
