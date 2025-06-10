2b02bc0abc2b11cbef8504974543256c84784f01
Ersamey <meiliaersa@gmail.com>
2025-06-10 10:36:11 +0700

    edit akun

--- kelola.php
+++ kelola.php
@@ -1,20 +1,71 @@
-<?
-    include 'koneksi.php';
+<?php
+include 'koneksi.php';
+//ambil data akun untuk diubah
+$data = [];
+// Cek apakah ada parameter 'ubah' di URL
+// Jika ada, ambil data akun berdasarkan ID
+// Jika tidak, tampilkan form untuk menambahkan akun baru
+if (isset($_GET['ubah'])) {
+    $id = $_GET['ubah'];
+    $query = "SELECT * FROM tb_akun WHERE id = '$id'";
+    $result = mysqli_query($db, $query);
+    if ($result && mysqli_num_rows($result) > 0) {
+        $data = mysqli_fetch_assoc($result);
+    } else {
+        echo "Data tidak ditemukan.";
+        exit;
+    }
+} else {
+    // Jika tidak ada parameter 'ubah', tampilkan form untuk menambahkan akun baru
+    $data = [
+        'id' => '',
+        'username' => '',
+        'password' => '',
+        'nama' => '',
+        'email' => '',
+        'role' => ''
+    ];
+}
+// Cek apakah ada aksi yang dilakukan
+if (isset($_POST['aksi'])) {
+    if ($_POST['aksi'] == 'add') {
+        $username = $_POST['usn'];
+        $password = $_POST['pw'];
+        $nama = $_POST['nama'];
+        $email = $_POST['email'];
+        $role = $_POST['role'];
 
-    if (isset($_GET["ubah"])) {
-        $id = $_GET["ubah"];
+        $query_insert = "INSERT INTO tb_akun (id, username, password, nama, email, role) VALUES(null, '$username', '$password', '$nama', '$email', '$role')";
+        $sql = mysqli_query($db, $query_insert);
 
-        $query = "SELECT * FROM akun WHERE id = '$id';";
+        if ($sql) {
+            header("location:akun.php");
+        } else {
+            echo "Gagal menambahkan data.";
+        }
+    } elseif ($_POST["aksi"] == 'edit') {
+        $id = $_POST['id'];
+        $username = $_POST['usn'];
+        $password = $_POST['pw'];
+        $nama = $_POST['nama'];
+        $email = $_POST['email'];
+        $role = $_POST['role'];
+
+        $query = "UPDATE tb_akun SET username = '$username', password = '$password', nama = '$nama', email = '$email', role = '$role' WHERE id='$id'";
         $result = mysqli_query($db, $query);
 
-        if ($result && mysqli_num_rows($result) > 0) {
-            $data = mysqli_fetch_assoc($result);
+        if ($result) {
+            header("location:akun.php");
+        } else {
+            echo "Gagal mengubah data.";
         }
     }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,61 +73,69 @@
     <script>
         /* Set the width of the sidebar to 250px (show it) */
         function openNav() {
-        document.getElementById("mySidepanel").style.width = "250px";
+            document.getElementById("mySidepanel").style.width = "250px";
         }
 
         /* Set the width of the sidebar to 0 (hide it) */
         function closeNav() {
-        document.getElementById("mySidepanel").style.width = "0";
+            document.getElementById("mySidepanel").style.width = "0";
         }
     </script>
     <style>
-        *{
+        * {
             margin: 0;
             padding: 0;
         }
 
-        body{
+        body {
             background-color: #736153;
         }
 
-        .nav{
+        .nav {
             display: flex;
             justify-content: space-between;
             padding: 15px;
             background-color: #bb874f;
         }
 
-        .logo{
+        .logo {
             display: flex;
             flex-direction: row;
             justify-content: center;
             align-items: center;
         }
 
-        .logo a{
+        .logo a {
             text-decoration: none;
             color: #111;
         }
 
-        a img{
+        a img {
             width: 50px;
         }
 
         .sidepanel {
-            height: 250px; /* Specify a height */
-            width: 0; /* 0 width - change this with JavaScript */
-            position: fixed; /* Stay in place */
-            z-index: 1; /* Stay on top */
+            height: 250px;
+            /* Specify a height */
+            width: 0;
+            /* 0 width - change this with JavaScript */
+            position: fixed;
+            /* Stay in place */
+            z-index: 1;
+            /* Stay on top */
             top: 0;
             right: 0;
-            background-color: #111; /* Black*/
-            overflow-x: hidden; /* Disable horizontal scroll */
-            padding-top: 60px; /* Place content 60px from the top */
-            transition: 0.5s; /* 0.5 second transition effect to slide in the sidepanel */
+            background-color: #111;
+            /* Black*/
+            overflow-x: hidden;
+            /* Disable horizontal scroll */
+            padding-top: 60px;
+            /* Place content 60px from the top */
+            transition: 0.5s;
+            /* 0.5 second transition effect to slide in the sidepanel */
         }
 
-            /* The sidepanel links */
+        /* The sidepanel links */
         .sidepanel a {
             padding: 8px 8px 8px 32px;
             text-decoration: none;
@@ -86,12 +145,12 @@
             transition: 0.3s;
         }
 
-            /* When you mouse over the navigation links, change their color */
+        /* When you mouse over the navigation links, change their color */
         .sidepanel a:hover {
             color: #f1f1f1;
         }
 
-            /* Position and style the close button (top right corner) */
+        /* Position and style the close button (top right corner) */
         .sidepanel .closebtn {
             position: absolute;
             top: 0;
@@ -100,7 +159,7 @@
             margin-left: 50px;
         }
 
-            /* Style the button that is used to open the sidepanel */
+        /* Style the button that is used to open the sidepanel */
         .openbtn {
             font-size: 20px;
             cursor: pointer;
@@ -113,8 +172,8 @@
         .openbtn:hover {
             background-color: #444;
         }
-        
-        .isi{
+
+        .isi {
             padding: 30px;
             /* display: flex;
             flex-direction: column;
@@ -122,8 +181,8 @@
             align-items: center;
             background-color: magenta;
         }
-        
-        .fc{
+
+        .fc {
             background-color: red;
             display: flex;
             align-items: center;
@@ -135,11 +194,12 @@
             /* justify-content: end;
         } */
 
-        td label{
+        td label {
             font-size: 20px
         }
     </style>
 </head>
+
 <body>
     <div class="nav">
         <div class="logo">
@@ -160,88 +220,68 @@
     <div class="isi">
         <div class="judul">
             <?php
-                if(isset($_GET['ubah'])){
+            // Cek apakah ada parameter 'ubah' di URL. Jika ada, tampilkan judul untuk mengedit data, tampilkan form untuk mengedit data dengan data yang sudah ada
+            if (isset($_GET['ubah'])) {
+                echo "<h1>Edit Data Akun</h1>";
+            } else {
+                echo "<h1>Tambah Data Akun</h1>";
+            }
             ?>
-            <h1>edit data : </h1>
-            <p>edit yang diinginkan</p>
-            <?php
-                } else {
-            ?>
-            <h1>tambahkan data</h1>
-            <p>isi formulir</p>
-            <?php
-                }
-            ?>
+            <!-- // Form untuk menambahkan atau mengedit data akun -->
+            <table>
+                <form action="" method="post">
+                    <tr>
+                        <td><label for="id">ID</label></td>
+                        <td><input type="text" name="id" id="id" value="<?php echo $data['id']; ?>" readonly></td>
+                    </tr>
+                    <tr>
+                        <td><label for="usn">Username</label></td>
+                        <td><input type="text" name="usn" id="usn" value="<?php echo $data['username']; ?>" required></td>
+                    </tr>
+                    <tr>
+                        <td><label for="pw">Password</label></td>
+                        <td><input type="password" name="pw" id="pw" value="<?php echo $data['password']; ?>" required></td>
+                    </tr>
+                    <tr>
+                        <td><label for="nama">Nama</label></td>
+                        <td><input type="text" name="nama" id="nama" value="<?php echo $data['nama']; ?>" required></td>
+                    </tr>
+                    <tr>
+                        <td><label for="email">Email</label></td>
+                        <td><input type="email" name="email" id="email" value="<?php echo $data['email']; ?>" required></td>
+                    </tr>
+                    <tr>
+                        <td><label for="role">Role</label></td>
+                        <td><select name="role" id="role">
+                                <option value="" disabled selected>Pilih Role</option>
+                                <option value="admin" <?php if ($data['role'] == 'admin') echo 'selected'; ?>>Admin</option>
+                                <option value="kasir" <?php if ($data['role'] == 'kasir') echo 'selected'; ?>>Kasir</option>
+                            </select></td>
+                    </tr>
+            </table>
+            <input type="hidden" name="id" value="<?php echo $data['id']; ?>">
+            <input type="hidden" name="aksi" value="<?php echo isset($_GET['ubah']) ? 'edit' : 'add'; ?>">
+
+
         </div>
-        <form method="POST" action="proses.php">
-            <?php if(isset($_GET['ubah'])): ?>
-                <input type="text" name="id" value="<?= $data['id'] ?>">
-            <?php endif; ?>
-            <div class="fc">
-                <table>
-                    <tr>
-                        <td>
-                            <label for="usn">Username :</label>
-                        </td>
-                        <td>
-                            <input required type="text" id="usn" name="usn" placeholder="minimal 2 huruf">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="nama">Nama :</label>
-                        </td>
-                        <td>
-                            <input required type="text" id="nama" name="nama" placeholder="minimal">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="pw">Password :</label>
-                        </td>
-                        <td>
-                            <input required type="password" name="pw" id="pw">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="email">Email :</label>
-                        </td>
-                        <td>
-                            <input required type="email" name="email" id="email">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="role">Pilih Role</label>
-                        </td>
-                        <td>
-                            <select required name="role" id="role">
-                                <option selected>Pilih role</option>
-                                <option value="admin">admin</option>
-                                <option value="kasir">kasir</option>
-                            </select>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div class="aksi">
-                <?php 
-                    if(isset($_GET['ubah'])){
-                ?>
+        <div class="aksi">
+            <?php
+            if (isset($_GET['ubah'])) {
+            ?>
                 <button type="submit" name="aksi" value="edit">Edit Perubahan</button>
-                <?php 
-                } else {
-                ?>
+            <?php
+            } else {
+            ?>
                 <button type="submit" name="aksi" value="add">Tambahkan Perubahan</button>
-                <?php 
-                }
-                ?>
-                <a href="akun.php">
-                    <input type="button" value="batal">
-                </a>
-            </div>
+            <?php
+            }
+            ?>
+            <a href="akun.php">
+                <input type="button" value="batal">
+            </a>
+        </div>
         </form>
     </div>
 </body>
+
 </html>
